8c5852d1dbfbf98af2f35da037e64a83c9fa2255
Min Yang (InConsulting Inc.) <v-miyan@microsoft.com>
2018-07-29 15:10:23 -0700

    initial commit - added book class: - constructor methods - get and set methods - private attributes

---
+++ CodePracticeOO/Color.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodePracticeOO
+{
+    class Color
+    {
+        private int red;
+        private int blue;
+        private int green;
+        private int alpha;
+
+        public Color(int red, int blue, int green)
+        {
+            this.red = red;
+            this.blue = blue;
+            this.green = green;
+        }
+
+        public Color(int red, int blue, int green)
+        {
+            this.red = red;
+            this.blue = blue;
+            this.green = green;
+            this.alpha = 255;
+        }
+
+        public int GetRed()
+        {
+            return red;
+        }
+
+        public int GetBlue()
+        {
+            return blue;
+        }
+
+        public int GetGreen()
+        {
+            return green;
+        }
+
+        public void SetRed(int red)
+        {
+            this.red = red;
+        }
+
+        public void SetBlue(int blue)
+        {
+            this.blue = blue;
+        }
+
+        public void SetGreen(int green)
+        {
+            this.green = green;
+        }
+
+        public float GetGrayscale()
+        {
+            return (red + blue + green) / 3.0f;
+        }
+    }
+}
---
+++ CodePracticeOO/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodePracticeOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var random = new Random();
+            //int dieRoll = random.Next(6) + 1;
+            //Console.WriteLine(dieRoll);
+            //Console.ReadKey();
+        }
+    }
+}
