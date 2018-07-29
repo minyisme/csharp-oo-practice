9ab070e5fd4d860d9c63d88160cad557787a9c51
Min Yang (InConsulting Inc.) <v-miyan@microsoft.com>
2018-07-29 15:28:58 -0700

    added ball class and wrote program to instantiate and throw them: - ball class: based on color and size - ball class: can pop and throw and show thrown times - program: create three colors and one ball of each color - program: throw balls, pop balls, and print to console ball thrown times

---
+++ CodePracticeOO/Ball.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodePracticeOO
+{
+    class Ball
+    {
+        private int size;
+        private Color color;
+        private int timesThrown;
+
+        public Ball(int size, Color color)
+        {
+            this.size = size;
+            this.color = color;
+            this.timesThrown = 0;
+        }
+
+        public void Pop()
+        {
+            this.size = 0;
+        }
+
+        public void Throw()
+        {
+            if (this.size != 0)
+            {
+                this.timesThrown += 1;
+            }
+        }
+
+        public int DisplayTimesThrown()
+        {
+            return this.timesThrown;
+        }
+
+    }
+}
--- CodePracticeOO/Program.cs
+++ CodePracticeOO/Program.cs
@@ -14,6 +14,40 @@ namespace CodePracticeOO
             //int dieRoll = random.Next(6) + 1;
             //Console.WriteLine(dieRoll);
             //Console.ReadKey();
+
+            var blue = new Color(0, 255, 0);
+            var red = new Color(255, 0, 0);
+            var green = new Color(0, 0, 255);
+            var blueBall = new Ball(10, blue);
+            var redBall = new Ball(5, red);
+            var greenBall = new Ball(15, green);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                blueBall.Throw();
+            }
+            
+            for (int i = 1; i <= 5; i++)
+            {
+                redBall.Throw();
+            }
+
+            for (int i = 1; i <= 15; i++)
+            {
+                greenBall.Throw();
+            }
+
+            blueBall.Pop();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                blueBall.Throw();
+            }
+
+            Console.WriteLine($"Blue ball has been thrown {blueBall.DisplayTimesThrown()} times");
+            Console.WriteLine($"Red ball has been thrown {redBall.DisplayTimesThrown()} times");
+            Console.WriteLine($"Green ball has been thrown {greenBall.DisplayTimesThrown()} times");
+            Console.ReadKey();
         }
     }
 }
