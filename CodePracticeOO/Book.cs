9f5de09328260d86cc6be6af0a7f4e01aa0b8ffd
Min Yang (InConsulting Inc.) <v-miyan@microsoft.com>
2018-07-29 16:02:15 -0700

    added other autogenerated files

---
+++ CodePracticeOO/Book.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodePracticeOO
+{
+    class Book
+    {
+        private string title;
+        private string author;
+        private int pages;
+        private int wordCount;
+
+        public Book(string title, string author)
+        {
+            this.title = title;
+            this.author = author;
+        }
+
+        // what happens here with 2 constructors, what if you used diff info for this.title? what takes precedence?
+        public Book(string title, string author, int pages, int wordCount)
+        {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+            this.wordCount = wordCount;
+        }
+
+        public string GetTitle()
+        {
+            // why doesn't this need a "this" similar to python's "self"?
+            return title;
+        }
+
+        public void SetTitle(string title)
+        {
+            this.title = title;
+        }
+
+        public void AssignWordCountFromText(string text)
+        {
+            wordCount = text.Split(' ').Length;
+        }
+    }
+}
